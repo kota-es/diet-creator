8ceed7542a09b4588e06f959762144c756e81542
kota-es <info@k-3110.com>
2019-11-28 11:48:55 +0900

    ユーザー画像アップロード機能仮実装

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 2019_11_26_112345) do
     t.string "email", null: false
     t.string "crypted_password", null: false
     t.string "salt"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
