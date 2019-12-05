5d3476b329057de2eb1bcc6ca8a9fd5ef4bb5e3d
kota-es <info@k-3110.com>
2019-12-05 11:02:41 +0900

    商品詳細ページ（簡易）作成

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
   include CarrierWave::MiniMagick
-  process resize_to_limit: [200, 200]
   # Choose what kind of storage to use for this uploader:
   if Rails.env.development? || Rails.env.test?
     storage :file
