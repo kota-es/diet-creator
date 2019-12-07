54445007104cdc876cd3022774752734828e7f7d
kota-es <info@k-3110.com>
2019-12-08 00:37:12 +0900

    数量を削除

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,13 +2,10 @@ class ListsController < ApplicationController
   before_action :setup_list_item, only: [:add_item]
 
   def add_item
-    if @list_item.blank?
-      @list_item = current_list.list_items.new(item_id: params[:item_id])
-    end
-
-    @list_item.quantity += 1
+    @list_item = current_list.list_items.new(item_id: params[:item_id])
     @list_item.save
-    redirect_to root_path
+    flash[:notice] =  "#{@list_item.item.name}を食事予定に追加しました"
+    redirect_to items_path
   end
 
   private
