4c0768ab4c113a253a8900073e019a65a9d04f8c
kota-es <info@k-3110.com>
2019-12-07 15:46:30 +0900

    商品削除の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 class ItemsController < ApplicationController
 
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @items = Item.all.order(params[:sort])
+    @items = Item.includes(:shops).order(params[:sort])
   end 
     
   def new
@@ -11,23 +11,10 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = current_user.items.new(item_params
-      # name: item_params[:name],
-      # image: item_params[:image],
-      # genre_id: item_params[:genre_id].to_i,
-      # shop_id: item_params[:shop_id].to_i,
-      # price: item_params[:price],
-      # kcal: item_params[:kcal],
-      # protein: item_params[:protein],
-      # fat: item_params[:fat],
-      # carb: item_params[:carb],
-      # salt: item_params[:salt],
-      # note: item_params[:note],
-      # user_id: current_user.id
-      )
+    @item = current_user.items.new(item_params)
     if @item.save
       flash[:notice] = "商品「#{@item.name}」の登録が完了しました"
-      redirect_to root_path
+      redirect_to items_path
     else
       render :new
     end
@@ -43,10 +30,15 @@ class ItemsController < ApplicationController
   def update
     if @item.update(item_params)
       flash[:notice] = "商品「#{@item.name}」の更新が完了しました"
-      redirect_to root_path
+      redirect_to items_path
     else
       render :edit
     end
+  end
+
+  def destroy
+    @item.destroy if @item.user_id == current_user.id
+    redirect_to items_path
   end
 
   private
