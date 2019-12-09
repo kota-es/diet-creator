8539bc50e2e3f483bc100e55c4c6257f76f24af7
kota-es <info@k-3110.com>
2019-12-09 11:54:23 +0900

    食事予定リストの実装

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,22 @@
 class ListsController < ApplicationController
   before_action :setup_list_item, only: [:add_item]
 
+  def show
+    @lists = current_list.list_items.all
+  end
+
   def add_item
     @list_item = current_list.list_items.new(item_id: params[:item_id])
     @list_item.save
     flash[:notice] =  "#{@list_item.item.name}を食事予定に追加しました"
     redirect_to items_path
+  end
+
+  def delete_item
+    @list_item = current_list.list_items.find(params[:id])
+    @list_item.destroy 
+    flash[:notice] =  "#{@list_item.item.name}を食事予定から削除しました"
+    redirect_to list_path(current_list)
   end
 
   private
