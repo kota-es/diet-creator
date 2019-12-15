0039ea308330a70f8c13d5cb53d809a0d109edaa
kota-es <info@k-3110.com>
2019-12-15 16:11:22 +0900

    食事記録作成メソッドの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :require_login
-
   helper_method :current_list
+
 
   def current_list
     begin
@@ -11,6 +11,13 @@ class ApplicationController < ActionController::Base
       list = List.create(user_id: current_user.id)
       session[:list_id] = list.id
       return list
+    end
+  end
+
+  def set_up_record
+    record = Record.find_by(user_id: current_user.id)
+    if record.blank?
+      Record.create(user_id: current_user.id)
     end
   end
 
